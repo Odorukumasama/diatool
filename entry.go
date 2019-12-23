e02f8d05b99155e96fb267c0ede4d2649d4a4857
Odorukumasama <59180039+Odorukumasama@users.noreply.github.com>
2019-12-23 22:03:07 +0100

    Added struct factory

--- entry.go
+++ entry.go
@@ -3,21 +3,22 @@ package main
 import (
 	//"encoding/json"
 	"fmt"
-	"time"
+	//"time"
 )
 
 type entry struct {
-	id   int
-	date time.Time
+	id int
+	//date time.Time
 }
 
-func (e *entry) NewEntry(id int, date time.Time) *entry {
-	e1 := new(entry)
-	e1.id = 1
+func NewEntry() *entry {
+	e := new(entry)
+	e.id = 1
 
-	return &e1{id}
+	return e
 }
 
 func main() {
-
+	eintrag1 := NewEntry()
+	fmt.Printf("Id lautet: %v\n", eintrag1.id)
 }
