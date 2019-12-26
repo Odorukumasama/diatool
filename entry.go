e2f64ca1a4d8bf8d0fc30e4b1a8cd7fa9b1a6608
Odorukumasama <59180039+Odorukumasama@users.noreply.github.com>
2019-12-27 00:57:01 +0100

    Test with json data

--- entry.go
+++ entry.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
-	//"time"
+	"time"
 )
 
 type entry struct {
-	id int
-	//date time.Time
+	id   int
+	date time.Time
 }
 
-func NewEntry() *entry {
+func NewEntry(id int) *entry {
 	e := new(entry)
-	e.id = 1
+	e.id = id
+
+	e.date = time.Now()
 
 	return e
 }
 
 func main() {
-	eintrag1 := NewEntry()
-	fmt.Printf("Id lautet: %v\n", eintrag1.id)
+	eintrag1 := NewEntry(1)
+	fmt.Printf("Id eintrag1 lautet: %v\n", eintrag1.id)
+	fmt.Printf("Date eintrag1 lautet: %v\n", eintrag1.date)
+
+	eintrag2 := NewEntry(2)
+	fmt.Printf("Id eintrag2 lautet: %v\n", eintrag2.id)
+	fmt.Printf("Date eintrag2 lautet: %v\n", eintrag2.date)
+
+	var jsonData []byte
+	jsonData, err := json.Marshal(eintrag1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(jsonData))
+
 }
